a1e09c7d751048a252b8630c8aee5b52b96ca26b
CHRIS BLACKSTOCK <crblacks@us.ibm.com>
2025-10-27 16:44:23 -0500

    script

--- terraform/templates/ansible-userdata.sh
+++ terraform/templates/ansible-userdata.sh
@@ -15,6 +15,12 @@ source /home/ubuntu/webui-venv/bin/activate
 # Upgrade pip and install Ansible
 pip install --upgrade pip setuptools wheel ansible
 
+# Clone the repo root 
+git clone ${ansible_repo_url} /home/ubuntu/ansible-repo
+
+# Install Ansible in the venv 
+sudo apt install ansible -y
+
 # Run the Ansible playbook from the ansible-repo
 cd /home/ubuntu/ansible-repo/ansible
 ANSIBLE_PYTHON_INTERPRETER=/home/ubuntu/webui-venv/bin/python \
